b40f98e4c5b1e69404823516d9f04ef77a0682fa
Qoholbohe <zqo14358407507233@163.com>
2021-04-17 15:05:52 +0800

    update

--- Cup.java
+++ Cup.java
@@ -1,11 +1,38 @@
-import java.util.Scanner;
-
-public class Cup {
-	public int get(){
-	System.out.println("输入骰子个数:");
-	int count;
-	Scanner sc = new Scanner(System.in);
-	count = sc.nextInt();
-	return count;
-	}
+public class Cup 
+{
+    private int num;
+    @SuppressWarnings("unused")
+    private Dice[] dice;
+    
+    public Cup(int num)
+    {
+        this.num = num; 
+        dice = new Dice [num];
+        for(int i = 0; i < num; i++)
+        {
+            dice[i] = new Dice();
+            
+        }
+    }
+    
+    public void setNum(int num)
+    {
+        this.num = num;
+    }
+    
+    public int getNum()
+    {
+        return num;
+    }
+    
+    public void rollPrint()
+    {
+        
+        for(int i = 0; i < num; i++)
+        {
+        	
+            System.out.print("第" + (i+1) + "个骰子的面值:" + dice[i].getFaceValue());
+            System.out.println();
+        }
+    }
 }
--- Test.java
+++ Test.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 
-public class Test
+public class Test 
 {
-	public static void main(String[] args) 
-	{
-		Dice d1 = new Dice();
-		d1.print();	
-		d1.printSum();
-	}
-}
+    public static void main(String[] args) 
+    {
+        int num;
+        System.out.println("请输入骰子的个数:");
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        
+        Cup cup = new Cup(num);
+        cup.rollPrint();
+    }
 
+}
